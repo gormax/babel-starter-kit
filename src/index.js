db069b4c0169a7f5ec15b6d0a9f0f5c192e1762e
gormax <m.kandid@gmail.com>
2016-11-13 00:46:54 +0300

    решено

--- src/index.js
+++ src/index.js
@@ -3,9 +3,37 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+
 app.get ('/task2A', (req, res) =>{
     const sum = (+req.query.a || 0)+ (+req.query.b || 0);
     res.send(sum.toString());
+});
+
+app.get ('/task2B', (req, res) =>{
+
+    const strFIO = req.query.fullname;
+    var arrFIO = strFIO.split(' ');
+    var strRes = '';
+
+    console.log(arrFIO.length.toString());
+
+    if (arrFIO.length.toString() == '3') {
+      strRes = arrFIO[2] + ' ' + arrFIO[0][0] + '. ' + arrFIO[1][0] + '.';
+    }
+    else if (arrFIO.length.toString() == '2'){
+      strRes = arrFIO[1] + ' ' + arrFIO[0][0] + '.';
+    }
+    else if ((arrFIO.length.toString() == '1') && !(arrFIO[0] == '')){
+      strRes = arrFIO[0];
+    }
+    else{
+      strRes = 'Invalid fullname';
+    }
+
+    res.send(strRes);
+
+    // console.log(strRes);
+    // res.send(arrFIO.length.toString());
 });
 
 app.listen(3000, () =>{
