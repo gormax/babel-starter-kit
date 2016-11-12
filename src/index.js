4667835ee2615d12e31e79bd867a28ffdc9f25f1
gormax <m.kandid@gmail.com>
2016-11-13 00:00:58 +0300

    решено

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get ('/task2A', (req, res) =>{
+    const sum = (+req.query.a || 0)+ (+req.query.b || 0);
+    res.send(sum.toString());
+});
+
+app.listen(3000, () =>{
+  console.log('ok');
+});
